5db1441a83020342ee93ea382fa89c5ee62b1a7e
Yang Chen <yang.chen@answerrocket.com>
2025-02-02 07:54:06 -0700

    add old shipping sticker generator back

--- streamlitController.py
+++ streamlitController.py
@@ -59,10 +59,68 @@ def main():
             file_name=updated_xlsx_name,
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
+    # shipping_sticker_generator_v1
+    st.divider()
+    st.header(':blue[Shipping Sticker] Generator - Google Sheets 🚚')
+    st.markdown("⚠️ Source Table: Google Sheets")
+    # Display the shipping template file for double-checking
+    st.subheader("Step1: Upload Shipping Sticker Template",divider="blue")
+   
+    # File uploader for the new template
+    new_template_file = st.file_uploader(":blue[Upload template.ppt]", type="pptx")
+    prs_file = Presentation(new_template_file)
+    
+    st.subheader("Step2: Upload Shipping Data",divider="blue")
+    uploaded_shipping_file = st.file_uploader(":blue[Upload shippingdata.csv]", type="csv")
+
+    if uploaded_shipping_file is not None:
+        df_shipping = process_shipping_data(uploaded_shipping_file)
+
+        sticker_generate_button = st.button("Generate Stickers")
+        if sticker_generate_button:
+            # Load client servings from Google Sheets
+            sheet_id = "1rorOBlH_K9qH4L39KehvI_rYGHo7agNVdCDisWydEj8"
+            LA_sheet_name = "ClientServings-LA"
+            NY_sheet_name = "ClientServings"
+            client_sheet_name = "Clients"
+
+            LA_clientservings_df = fetch_client_servings(sheet_id, LA_sheet_name)
+            NY_clientservings_df = fetch_client_servings(sheet_id, NY_sheet_name)
+
+            # Combine LA and NY client servings
+            all_client_servings = pd.concat([LA_clientservings_df, NY_clientservings_df], ignore_index=True)
+
+            # Fetch package recipent from Client
+            package_recipient_df = fetch_package_recipient(sheet_id, client_sheet_name)
+
+            # Attach package recipent to client servings
+            updated_all_client_servings = add_recipient_clientservings(all_client_servings,package_recipient_df)
+
+            # Match orders with shipping info
+            final_match_result_with_portion_df = match_orders_to_shipping_data(updated_all_client_servings, df_shipping)
+
+            # Generate the PowerPoint file
+            prs =generate_ppt_v1(final_match_result_with_portion_df, prs_file)
+
+            # Optional: Display the final DataFrame for checking
+            st.subheader("Shipping Stickers")
+            st.markdown(":green[Stickers are ready! Click to download]")
+            updated_ppt_name = f'{current_date}_shipping_sticker_updated.pptx'
+            prs.save(updated_ppt_name)
+            with open(updated_ppt_name, "rb") as file:
+                st.download_button(
+                    label="Download Stickers",
+                    data=file,
+                    file_name=updated_ppt_name,
+                    mime="application/vnd.openxmlformats-officedocument.presentationml.presentation"
+                )
+            st.caption(":orange[This is the final table which is fed to sticker ppt, for review purpose]")
+            st.dataframe(final_match_result_with_portion_df)
+
 
     # shipping_sticker_generator_v2
     st.divider()
-    st.header(':blue[Shipping Sticker] Generator 🚚')
+    st.header(':blue[Shipping Sticker] Generator 🚚 - Airtable')
     st.markdown("⚠️ Source Table: [Open Orders > All](https://airtable.com/appEe646yuQexwHJo/tblxT3Pg9Qh0BVZhM/viwICiVvohvm7Zfuo?blocks=hide)")
     st.markdown("⚠️ If noticed any issues or missing data, please first check data in source table and then re-run the generator")
     new_template_file_v2 = st.file_uploader(":blue[Optionally Upload template.pptx]", type="pptx",key="new_template_file_v2")
